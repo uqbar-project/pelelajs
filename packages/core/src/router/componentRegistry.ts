9017ea777a518baf68147d8c1ca3e2b8f4a87399
fdodino <fernando.dodino@gmail.com>
2026-06-28 16:54:09 -0300

    Code Rabbit fixes

--- packages/core/src/router/componentRegistry.ts
+++ packages/core/src/router/componentRegistry.ts
@@ -3,17 +3,11 @@ import {
   defineComponent as defineComponentOriginal,
   getComponentEntry,
 } from '../registry/componentRegistry'
+import { toKebabCase } from '../commons/helpers'
 import type { ViewModelConstructor } from '../types'
 
-function camelToKebab(name: string): string {
-  return name
-    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
-    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1-$2')
-    .toLowerCase()
-}
-
 export function inferComponentName(creator: ViewModelConstructor): string {
-  return camelToKebab(creator.name)
+  return toKebabCase(creator.name)
 }
 
 export function autoRegisterComponent(creator: ViewModelConstructor, template: string): void {
